309e455161964ed3167a060813d539d43e0ef492
noahming <nm99@gmx.ch>
2017-07-19 16:25:42 +0200

    EndOfDay3

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -67,10 +68,10 @@ class BlogController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Request $request)
+    public function show(Request $request, $id)
     {
-        echo $request->segment(2);
-        return view('post');
+        $blog = Blog::all()->where('id', $id)->first();
+        return view('post', ['blog' => $blog]);
     }
 
     /**
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,16 +6,19 @@
 <body>
 @include('header')
 <div id="bigBlogContainer">
-    <h1>{{ $blog->title }}</br><small>by {{ $blog->user->username }} {{ $blog->date }}</small></h1>
+    <h1>{{ $blog->title }}</br>
+        <small>by {{ $blog->user->username }} {{ $blog->date }}</small>
+    </h1>
     <div class="form-group">
         <input class="form-control" disabled value="{{$blog->desc_short}}"/>
     </div>
     <div class="form-group">
         <input class="form-control" disabled value="{{$blog->desc_long}}"/>
     </div>
+    @foreach ($blog->images as $image)
+        <img style="width: 80%; margin-left: 10%; margin-right: 10%"
+             src="{{asset("storage/images/".$image->filename.'.'.$image->image_type)}} "/>
+    @endforeach
 </div>
 </body>
 </html>
-<?php
-$blog = Blog::all()->where('id', )->first();
-        ?>
